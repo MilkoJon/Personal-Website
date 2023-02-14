b17d2a990783c2b80e61557c9e4b10dd5e2b94db
unknown <milko@live.com>
2023-02-14 11:10:07 +0100

    side hover

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ hamburger.addEventListener("click", () => {
 
 // Personal Info // About Me
 function printLetterByLetter(destination, message, speed) {
-    console.log(123455)
+  console.log(123455);
   var i = 0;
   var interval = setInterval(function () {
     document.getElementById(destination).innerHTML += message.charAt(i);
@@ -34,8 +34,75 @@ function printLetterByLetter(destination, message, speed) {
 }
 var message =
   "“Civil Engineer with international experience looking for entry-level positions in the IT industry. Highly motivated, just finished an intensive JavaScript React bootcamp. Excited for new opportunities, ready to dedicate and always eager to learn more and improve as much as possible.     Why do I see myself in the IT industry? I like challenges and enjoy solving problems. All my previous contacts with programming I enjoyed and it was something that drives me. I am also a big math lover. I have successfully created buildings in the construction industry, why not do the same in the IT industry?”";
-// var messageElement = document.getElementById("aboutMe");
-// console.log(messageElement)
-
 var messageElement = "aboutMe";
 // printLetterByLetter(messageElement, message, 10);
+
+// Education & Liecenses
+
+// put this in function
+var frontendBootcamp = document.querySelector(".bit");
+console.log(frontendBootcamp);
+frontendBootcamp.addEventListener("mouseenter", () => {
+  var bitSlide = document.createElement("div");
+  bitSlide.classList.add("bitSlide");
+  bitSlide.setAttribute("id", "sideHover");
+  var bitSertificateImg = document.createElement("img");
+  $(bitSertificateImg).attr("src", "./images/education/BITsertifikat.png");
+  var detailsEducationLiecenses = document.querySelector(
+    ".detailsEducationLiecenses"
+  );
+  bitSlide.append(bitSertificateImg);
+  detailsEducationLiecenses.append(bitSlide);
+});
+frontendBootcamp.addEventListener("mouseleave", () => {
+  var bitSlide = document.querySelector("#sideHover");
+  bitSlide.remove();
+});
+
+
+function sideHover(imagePath, pageClassToAppear) {
+    var div = document.createElement("div");
+    div.setAttribute("id", "sideHover");
+    var image = document.createElement("img");
+    $(image).attr("src", imagePath);
+    var pageToAppera = document.querySelector(pageClassToAppear);
+  div.append(image);
+  pageToAppera.append(div);
+}
+function removeSideHover() {
+    var x = document.querySelector("#sideHover");
+    x.remove();
+}
+
+var grfMaster = document.querySelector(".grf");
+grfMaster.addEventListener("mouseenter", () => {
+  sideHover(
+    "./images/education/grfMaster.png",
+    ".detailsEducationLiecenses"
+  );
+});
+grfMaster.addEventListener("mouseleave", () => {
+    removeSideHover()
+});
+
+var fimMaster = document.querySelector(".fim");
+fimMaster.addEventListener("mouseenter", () => {
+  sideHover(
+    "./images/education/fimMaster.png",
+    ".detailsEducationLiecenses"
+  );
+});
+fimMaster.addEventListener("mouseleave", () => {
+    removeSideHover()
+});
+
+var iksLicence = document.querySelector(".iks");
+iksLicence.addEventListener("mouseenter", () => {
+  sideHover(
+    "./images/education/iksLicence.png",
+    ".detailsEducationLiecenses"
+  );
+});
+iksLicence.addEventListener("mouseleave", () => {
+    removeSideHover()
+});
